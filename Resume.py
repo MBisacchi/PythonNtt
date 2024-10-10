2dc194a844cbef58b6084826238483efa5640e46
Mayla <99743609+MBisacchi@users.noreply.github.com>
2024-10-10 00:38:32 -0300

    Create Resume.py

---
+++ Resume.py
@@ -0,0 +1,24 @@
+# Operadores arrtiméticos
+
+# Adição
+print (1+1)
+
+# Subtração
+print (10-2)
+
+# Multiplicação
+print (4*3)
+
+# Divisão quebrada 
+print (12/3)
+
+# Divisão inteira
+print (12//2)
+
+# Modulo -> resto da divisão
+print (10%3)
+
+# Exponenciação -> o numero (x=2) ao cubo ou ao qudrado
+print (2**3)
+
+# segue as convenções das regras matemáticas (ex.: 1. (), 2. expoentes, 3. * e /, 4. + e -)
